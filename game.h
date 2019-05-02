b42752e0885ccfa4c04c0c4f824f60282624fb84
sermart1234 <ser.martinov.1234@gmail.com>
2019-05-02 19:37:34 +0300

    generation masMap

--- game.h
+++ game.h
@@ -1,11 +1,17 @@
 #ifndef GAME_H
 #define GAME_H
 
+#include "time.h"
+#include "stdlib.h"
+
+
 class Game {
 public:
     Game();
     int masMap[10][10];
     int masMapF[10][10];
     void newGame();
+    int checkAbout(int Map[10][10], int i, int j, bool horizontal, int sizeShip);
+    void genShips(int Map[10][10]);
 };
 #endif // GAME_H
